01dd12e663e22f121a928f009e28db3861b0ca96
Xuanhai244 <xuanhai.tienphong@gmail.com>
2023-06-03 14:34:48 +0700

    Add Service

---
+++ tpm.web.contract/Controllers/ContactsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tpm.web.contract.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
