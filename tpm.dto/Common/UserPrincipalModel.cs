bd0769a40265b8074b539d5f5eb7f26682c690f8
Xuanhai244 <xuanhai.tienphong@gmail.com>
2023-06-05 13:52:55 +0700

    Update Code (5/6)

--- tpm.dto/Common/UserPrincipalModel.cs
+++ tpm.dto/Common/UserPrincipalModel.cs
@@ -7,6 +7,9 @@ namespace tpm.dto.admin
         public int UserID { get; set; }
         public string UserName { get; set; }
         public string FullName { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }   
+        public string Email { get; set; }
 
     }
 }
