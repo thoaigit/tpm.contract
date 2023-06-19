531c440da28aeff935260ebf3adfb79347c8ebc2
Chunn Naa <lenguyentrungnam22@gmail.com>
2023-06-19 11:01:50 +0700

    chức năng seach và sửa giao diện

---
+++ tpm.web.contract/wwwroot/assets/js/contractIndex.js
@@ -0,0 +1,27 @@
+﻿
+	function search() {
+			var keyword = document.getElementById("keyword").value.toLowerCase();
+	var rows = document.getElementsByClassName("list-contract");
+	var found = false;
+
+	for (var i = 0; i < rows.length; i++) {
+				var rowData = rows[i].textContent.toLowerCase();
+	var rowDataWithoutDiacritics = removeVietnameseDiacritics(rowData);
+
+	if (rowData.includes(keyword) || rowDataWithoutDiacritics.includes(keyword)) {
+		rows[i].style.display = "table-row";
+	found = true;
+				} else {
+		rows[i].style.display = "none";
+				}
+			}
+
+	if (!found) {
+		alert("Không tìm thấy kết quả.");
+			}
+		}
+
+	function removeVietnameseDiacritics(str) {
+		str = str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+	return str;
+		}
---
+++ tpm.web.contract/wwwroot/assets/js/createContract.js
@@ -0,0 +1,21 @@
+﻿
+
+function openTab(event, tabName) {
+    var i, tabcontent, tablinks;
+
+    // Ẩn tất cả các tabcontent
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Xóa lớp active khỏi tất cả các tablinks
+    tablinks = document.getElementsByClassName("nav-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Hiển thị tabcontent và đánh dấu tablink được chọn
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
